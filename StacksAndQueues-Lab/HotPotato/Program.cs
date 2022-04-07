d6709d4e54ab9ef18f3c468b942ff8ae6c9dffd2
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-07 16:10:17 +0300

    Added new problem

---
+++ StacksAndQueues-Lab/HotPotato/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split().ToArray();
+           
+            Queue<string> queue = new Queue<string>(names);
+
+            int count = int.Parse(Console.ReadLine());
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    if (i < count)
+                    {
+                        queue.Enqueue(queue.Dequeue());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Removed {queue.Dequeue()}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
