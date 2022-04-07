01c0876572942b2e653b4c1afcf5ebd1743be47f
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-07 15:04:25 +0300

    Added new problem

---
+++ StacksAndQueues-Lab/SimpleCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Stack<string> stack = new Stack<string>(input);
+
+            int sum = 0;
+            int currNum = 0;
+
+            while (stack.Count > 0)
+            {
+                if (stack.Count % 2 != 0)
+                {
+                    currNum = int.Parse(stack.Pop());
+                }
+                else
+                {
+                    string symbol = stack.Pop();
+                    if (symbol == "+")
+                    {
+                        sum += currNum;
+                    }
+                    else if (symbol == "-")
+                    {
+                        sum -= currNum;
+                    }
+                }
+
+                if (stack.Count == 1)
+                {
+                    sum += int.Parse(stack.Pop());
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
