2951d6553fc511bbb2c468a9cb3ff2605862c573
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-07 15:22:43 +0300

    Added new problem

---
+++ StacksAndQueues-Lab/PrintEvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    queue.Enqueue(number);
+                }
+            }
+                
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
