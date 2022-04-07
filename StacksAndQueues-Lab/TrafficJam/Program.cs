dc8e056ab126800692d028d35f36ffe781ff95f1
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-07 16:19:49 +0300

    Added new problem

---
+++ StacksAndQueues-Lab/TrafficJam/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passCount = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            Queue<string> queue = new Queue<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    for (int i = 0; i < passCount; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            counter++;
+                        }
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+                
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
