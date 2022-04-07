7a54bc5c1f3cdbe7f335ca90571f7a8ae61ff63f
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-07 15:13:24 +0300

    Added new problem

---
+++ StacksAndQueues-Lab/MatchingBrackets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> indexStack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    indexStack.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = indexStack.Pop();
+                    int count = i - startIndex + 1;
+
+                    string expression = input.Substring(startIndex, count);
+
+                    Console.WriteLine(expression);
+                }
+            }
+        }
+    }
+}
