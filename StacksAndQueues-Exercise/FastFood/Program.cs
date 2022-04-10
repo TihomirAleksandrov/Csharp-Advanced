78af7d167836fc07f6d6f17261d3ca30a22b286d
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-10 16:04:57 +0300

    Added new problem

---
+++ StacksAndQueues-Exercise/FastFood/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(orders);
+
+            int biggestOrder = int.MinValue;
+
+            foreach (int order in queue)
+            {
+                if (order > biggestOrder)
+                {
+                    biggestOrder = order;
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                if (foodQuantity >= queue.Peek())
+                {
+                    int currOrder = queue.Dequeue();
+                    foodQuantity -= currOrder;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(biggestOrder);
+            
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+        }
+    }
+}
