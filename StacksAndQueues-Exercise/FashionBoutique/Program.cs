3335aaef92e3fa6aa6caae2cd1e7156ba8d6a6f9
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-10 16:23:15 +0300

    Added new problem

---
+++ StacksAndQueues-Exercise/FashionBoutique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(clothes);
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int rackCount = 1;
+            int currRack = 0;
+
+            while (stack.Count > 0)
+            {
+                int currCloth = stack.Pop();
+
+                if ((currCloth + currRack) <= rackCapacity)
+                {
+                    currRack += currCloth;
+                }
+                else
+                {
+                    currRack = currCloth;
+                    rackCount++;
+                }
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}
