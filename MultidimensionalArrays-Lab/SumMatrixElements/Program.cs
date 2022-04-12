1a404042e0a62bcfb4d446c3189aaa2ec5534b5d
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-12 22:13:04 +0300

    Added new problem

---
+++ MultidimensionalArrays-Lab/SumMatrixElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rowsNum = input[0];
+            int colsNum = input[1];
+
+            int[,] matrix = new int[rowsNum, colsNum];
+
+            for (int row = 0; row < rowsNum; row++)
+            {
+                input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < colsNum; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            Console.WriteLine(rowsNum);
+            Console.WriteLine(colsNum);
+
+            int sum = 0;
+
+            foreach (int num in matrix)
+            {
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
