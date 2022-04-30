aa7d5d1558bb2197587e616f87f8300530ede0a3
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-30 18:14:26 +0300

    Added new problem

---
+++ SetsAndDictionaries-Lab/Largest3Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] sortedNums = numbers.OrderByDescending(x => x).ToArray();
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (sortedNums.Length == i)
+                {
+                    break;
+                }
+
+                Console.Write($"{sortedNums[i]} ");
+            }
+        }
+    }
+}
