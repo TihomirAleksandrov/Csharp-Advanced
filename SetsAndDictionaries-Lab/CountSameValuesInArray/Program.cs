619c7fe5028b73c28119559f3b5ff7e481440020
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-04-30 17:43:13 +0300

    Added new problem

---
+++ SetsAndDictionaries-Lab/CountSameValuesInArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountSameValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            Dictionary<double, int> numbersCount = new Dictionary<double, int>();
+
+            foreach (double number in numbers)
+            {
+                if (numbersCount.ContainsKey(number))
+                {
+                    numbersCount[number]++;
+                }
+                else
+                {
+                    numbersCount.Add(number, 1);
+                }
+            }
+
+            foreach (var pair in numbersCount)
+            {
+                Console.WriteLine($"{pair.Key} - {pair.Value} times");
+            }
+        }
+    }
+}
