e5b8ae13deeaef1c148d3ec2ae27ead9e3aca95f
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-05-04 22:57:44 +0300

    Added new problem

---
+++ SetsAndDictionaries-Lab/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CitiesByContinentAndCountry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, List<string>>> cities = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int inputCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (cities.ContainsKey(continent))
+                {
+                    if (cities[continent].ContainsKey(country))
+                    {
+                        if (cities[continent][country].Contains(city))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            cities[continent][country].Add(city);
+                        }
+                    }
+                    else
+                    {
+                        cities[continent].Add(country, new List<string>());
+                        cities[continent][country].Add(city);
+                    }
+                }
+                else
+                {
+                    cities.Add(continent, new Dictionary<string, List<string>>());
+                    cities[continent].Add(country, new List<string>());
+                    cities[continent][country].Add(city);
+                }
+            }
+
+            foreach (string continent in cities.Keys)
+            {
+                Console.WriteLine($"{continent}:");
+
+                foreach (string country in cities[continent].Keys)
+                {
+                    Console.Write($"  {country} -> {string.Join(", ", cities[continent][country])}");
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
