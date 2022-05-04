20ebbe832481f64be61bfb01822f4c568533d7c4
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-05-04 23:12:26 +0300

    Added new problem

---
+++ SetsAndDictionaries-Lab/RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            
+            int inputNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
