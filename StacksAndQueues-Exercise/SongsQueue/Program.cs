4440ab3889005cd12ead5b08875102635ac1759b
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-05-29 22:03:54 +0300

    Added new problem

---
+++ StacksAndQueues-Exercise/SongsQueue/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstSongs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            
+            Queue<string> songs = new Queue<string>(firstSongs);
+
+            string input = Console.ReadLine();
+            while (songs.Count > 0)
+            {
+                string[] splitInput = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string command = splitInput[0];
+
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command == "Add")
+                {
+                    string song = input.Substring(4);
+
+                    if (songs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(song);
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
