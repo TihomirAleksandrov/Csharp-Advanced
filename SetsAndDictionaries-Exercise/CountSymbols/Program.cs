3b45a4fa1cb4be2dc07819057ba78dcc1ce92c2b
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-01 02:50:58 +0300

    Added new problem

---
+++ SetsAndDictionaries-Exercise/CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountSymbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> letters = new SortedDictionary<char, int>();
+
+            char[] lettersArr = Console.ReadLine().ToCharArray();
+
+            foreach (char c in lettersArr)
+            {
+                if (letters.ContainsKey(c))
+                {
+                    letters[c]++;
+                }
+                else
+                {
+                    letters.Add(c, 1);
+                }
+            }
+
+            foreach (var pair in letters)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value} time/s");
+            }
+        }
+    }
+}
