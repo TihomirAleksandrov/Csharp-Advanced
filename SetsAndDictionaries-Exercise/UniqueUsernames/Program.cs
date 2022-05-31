6e9902c2c3ad1a750d5c5849a8d389ef12659e07
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-01 02:19:17 +0300

    Added new problem

---
+++ SetsAndDictionaries-Exercise/UniqueUsernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+            int inputNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name);
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
