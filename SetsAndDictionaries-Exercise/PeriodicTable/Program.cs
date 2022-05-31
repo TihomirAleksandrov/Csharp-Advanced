2260ce79232ae311d9bcd9c38087bd44e6210eff
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-01 02:36:50 +0300

    Added new problem

---
+++ SetsAndDictionaries-Exercise/PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int inputNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                foreach (string element in input)
+                {
+                    elements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
