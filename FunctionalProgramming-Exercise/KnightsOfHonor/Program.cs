8860fe1d2da8abda8ed89dd8e82dae47d1404029
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-06 00:43:21 +0300

    Added new problem

---
+++ FunctionalProgramming-Exercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> print = name => Console.WriteLine($"Sir {name}");
+
+            foreach (string name in names)
+            {
+                print(name);
+            }
+        }
+    }
+}
