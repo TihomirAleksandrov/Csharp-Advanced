205b9e7b0ab7818d050a7a2f2d60e1c7aa5ae718
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-05 22:50:37 +0300

    Added new problem

---
+++ FunctionalProgramming-Lab/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> addVAT = x => x + x * 0.2;
+
+            double[] prices = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => double.Parse(x))
+                .Select(addVAT)
+                .ToArray();
+
+            foreach (var number in prices)
+            {
+                Console.WriteLine($"{number:f2}");
+            }
+        }
+    }
+}
