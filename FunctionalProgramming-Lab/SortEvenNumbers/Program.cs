dae8cb30cc16488554452f524264d8b40f214750
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-05 21:47:24 +0300

    Added new problems

---
+++ FunctionalProgramming-Lab/SortEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+
+            numbers = numbers.Where(x => x % 2 == 0).ToList();
+            numbers.Sort();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
