d3c57a0230909160f2ab02d9876b2e1805db792e
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-06 00:56:36 +0300

    Added new problem

---
+++ FunctionalProgramming-Exercise/CustomMinFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> func = x => x.Min();
+
+            int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+            Console.WriteLine(func(numbers));
+        }
+    }
+}
