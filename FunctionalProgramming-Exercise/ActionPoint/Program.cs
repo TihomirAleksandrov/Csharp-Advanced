4dab314794f750e55a7dba46375a1caa25114336
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-06 00:39:24 +0300

    Added new problems

---
+++ FunctionalProgramming-Exercise/ActionPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string[]> print = names => Console.WriteLine(string.Join("\n", names));
+
+            print(names);
+        }
+    }
+}
