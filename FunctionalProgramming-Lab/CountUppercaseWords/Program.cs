41d76c907f229537e79e7df7cd750281c3499587
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-05 22:01:06 +0300

    Added new problem

---
+++ FunctionalProgramming-Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> func = x => char.IsUpper(x[0]);
+
+            string[] text = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(func).ToArray();
+
+            foreach (string s in text)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming-Lab/SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToList();
+
+            int sum = numbers.Sum();
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(sum);
+        }
+    }
+}
