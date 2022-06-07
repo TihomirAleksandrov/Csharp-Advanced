c7484393f2f2e01036ad491272dbe1db59c633a5
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-07 19:39:05 +0300

    Added new problems

---
+++ FunctionalProgramming-Exercise/ReverseAndExclude/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+            int divisibleNum = int.Parse(Console.ReadLine());
+
+            numbers = CheckDivision(numbers, divisibleNum);
+            numbers = Reverse(numbers);
+            Print(numbers);
+        }
+
+        static int[] CheckDivision(int[] numbers, int divisibleNum)
+        {
+            Predicate<int> predicate = x => x % divisibleNum == 0;
+
+            int[] newNums = numbers.Where(x => !predicate(x)).ToArray();
+
+            return newNums;
+        }
+
+        static int[] Reverse(int[] numbers)
+        {
+            Array.Reverse(numbers);
+
+            return numbers;
+        }
+
+        static void Print(int[] numbers)
+        {
+            Console.Write(string.Join(" ", numbers));
+        }
+    }
+}
