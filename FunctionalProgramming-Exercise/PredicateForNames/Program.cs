2cf55329160c25f6eb578f35bbe183301ba106fe
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-07 20:30:13 +0300

    Added new problem

---
+++ FunctionalProgramming-Exercise/PredicateForNames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            names = FilterNames(names, nameLength);
+            Console.WriteLine(string.Join("\n", names));
+        }
+
+        static string[] FilterNames(string[] names, int nameLength)
+        {
+            Predicate<string> filter = x => x.Length <= nameLength;
+
+            string[] filteredNames = names.Where(x => filter(x)).ToArray();
+
+            return filteredNames;
+        }
+    }
+}
