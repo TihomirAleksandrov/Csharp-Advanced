ca23fa8667455ffd8f72ef183ab41722dba265bf
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-07 19:26:53 +0300

    Added new problems

---
+++ FunctionalProgramming-Exercise/AppliedAtithmetics/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace AppliedAtithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                switch (command)
+                {
+                    case "add":
+                        numbers = Add(numbers);
+                        break;
+                    case "multiply":
+                        numbers = Multiply(numbers);
+                        break;
+                    case "subtract":
+                        numbers = Subtract(numbers);
+                        break;
+                    case "print":
+                        Print(numbers);
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        static int[] Add(int[] numbers)
+        {
+            return numbers.Select(x => x + 1).ToArray();
+        }
+
+        static int[] Multiply(int[] numbers)
+        {
+            return numbers.Select(x => x * 2).ToArray();
+        }
+
+        static int[] Subtract(int[] numbers)
+        {
+            return numbers.Select(x => x - 1).ToArray();
+        }
+
+        static void Print(int[] numbers)
+        {
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
