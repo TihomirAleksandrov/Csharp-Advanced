927fac8bcd5944c60eab3bd9079a91e07587b583
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-07 20:57:12 +0300

    Added new problem

---
+++ FunctionalProgramming-Exercise/ListOfPredicates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            List<int> numbers = FillArray(range);
+
+            int[] dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            numbers = Filter(numbers, dividers);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        static List<int> FillArray(int range)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= range; i++)
+            {
+                numbers.Add(i);
+            }
+
+            return numbers;
+        }
+
+        static List<int> Filter(List<int> numbers, int[] dividers)
+        {
+            foreach (int divider in dividers)
+            {
+                Func<int, bool> filter = x => x % divider == 0;
+                numbers = numbers.Where(filter).ToList();
+            }
+
+            return numbers;
+        }
+    }
+}
