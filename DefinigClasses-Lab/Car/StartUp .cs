fe1eb16bd5ed4ca446397d58fe9aed70f4efbec2
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-20 14:04:17 +0300

    Added new projects

---
+++ DefinigClasses-Lab/Car/StartUp .cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp 
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "Mercedes";
+            car.Model = "C-class";
+            car.Year = 2009;
+
+            Car car1 = new Car()
+            {
+                Make = "Honda",
+                Model = "Civic",
+                Year = 2000
+            };
+
+            Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+        }
+    }
+}
---
+++ FunctionalProgramming-Exercise/PartyReservationFilterModule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartyReservationFilterModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
