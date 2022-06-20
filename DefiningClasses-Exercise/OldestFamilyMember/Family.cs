53e42e3791fa8fab326789808110ef4e3e68714a
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-20 22:24:29 +0300

    Added new solution

---
+++ DefiningClasses-Exercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public List<Person> Members
+        {
+            get { return members; }
+            set { members = value; }
+        }
+
+        public void AddMember(Person member)
+        {
+            this.members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return members.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
---
+++ DefiningClasses-Exercise/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Family members = new Family();
+            int inputNum = int.Parse(Console.ReadLine());
+            if (inputNum == 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                Person person = new Person(name, age);
+
+                members.AddMember(person);
+            }
+
+            Person oldestMember = members.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
