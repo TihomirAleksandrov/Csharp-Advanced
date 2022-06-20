8fa0f964c34da302f5cd3ae1062539d4cddb1104
Tihomir Aleksandrov <tihomir_rosenov_aleksandrov@abv.bg>
2022-06-20 14:24:22 +0300

    Added new project

---
+++ DefinigClasses-Lab/CarExtension/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Make = "Mazda",
+                Model = "MX-5",
+                Year = 2020,
+                FuelQuantity = 200,
+                FuelConsumption = 100
+            };
+
+            car.Drive(1);
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
